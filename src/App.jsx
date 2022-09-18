20210a272d930ba63c12f75d852035a771f8f3cd
zion livingstone <zionlivingstone@gmail.com>
2022-09-18 18:05:58 +0100

    commenting code

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,32 @@
-import React, { useState,useEffect } from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
 import './App.css'
 import categorydata from './category'
-import home from './Home.png'
 import Home from './Home'
-import {FaPlay,FaHistory,FaHome} from 'react-icons/fa'
+
 
 
 function App() {
   const [category,setcategory]=useState()
-  
-  const [questions,setquestions]=React.useState()
-  const [difficulty,setdifficulty]=React.useState()
-  const{byCategory,byDifficulty}=categorydata
+  const [difficulty,setdifficulty]=useState()
 
+ const{byCategory,byDifficulty}=categorydata//category data is imported from the category.jsx file that has json file that contains the categories and difficulties
 const difficulties=Object.keys(byDifficulty)
-  const categories=Object.keys(byCategory)
+ const categories=Object.keys(byCategory)
 
  function ApiCall(newcategorydifficulty) {
   if (categories.includes(newcategorydifficulty)) {
     const regex=/ & /
-    setcategory(newcategorydifficulty.replace(regex, '_and_' ))
+    setcategory(newcategorydifficulty.replace(regex, '_and_' ))// regex is used to make the string suitable for an api call by replacing some characters
 
   }
   if (difficulties.includes(newcategorydifficulty)) {
     setdifficulty(newcategorydifficulty)
   }
 
-}
+}//apicall gets an argument(newcategorydifficulty) from the Home.jsx component and sets the appropriate string to the category and difficulty state ,which are then sent as props back to home component
 
 
     return(
-      // <Frontpage change={changestart} categories={category} />
     <Home apiCall={ApiCall} categorydifficulty={[category,difficulty]} />
 
     )
